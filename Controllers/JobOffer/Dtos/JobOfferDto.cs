1285e0ce425cade74dfab43d89607ba0be819d16
Adomas02 <kazen.adomas@gmail.com>
2023-12-06 16:12:07 +0200

    removed string.Empty

--- Controllers/JobOffer/Dtos/JobOfferDto.cs
+++ Controllers/JobOffer/Dtos/JobOfferDto.cs
@@ -3,8 +3,8 @@
     public class JobOfferDto
     {
         public int Id { get; set; }
-        public string CustomerName { get; set; } = string.Empty;
-        public string CustomerPhoneNumber { get; set; } = string.Empty;
+        public string CustomerName { get; set; }
+        public string CustomerPhoneNumber { get; set; }
         public int jobId { get; set; }
         public string Status { get; set; } = string.Empty;
         public string Note {  get; set; } = string.Empty;
